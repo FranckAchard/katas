7ce376e75fafe3e5b47b34f56d4af7247154dfcf
Utilisateur <Utilisateur@BNPPF006>
2018-03-09 10:09:36 +0100

    sav

--- src/co/simplon/pf1/ReverseWords.java
+++ src/co/simplon/pf1/ReverseWords.java
@@ -1,9 +1,32 @@
 package co.simplon.pf1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ReverseWords {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String sentence= "salut les loulous ça va";
+		
+		/*
+		String reverseSentence="";
+		String[] listWords= sentence.split(" ");
+		ArrayList<String> reverseListWords= new ArrayList<String>();
+		
+		for (int i=listWords.length-1; i >= 0; --i) {
+			reverseListWords.add(listWords[i]);
+		}
+		
+		for (String word : reverseListWords) {
+			reverseSentence+= word + " ";
+		}
+				
+		System.out.println("[" + reverseSentence.trim() + "]");
+		*/
+		
+		// solution de fou codewars
+	    System.out.println(Arrays.stream(sentence.split(" ")).reduce((x, y) -> y+" "+x).get());
+	    
 
 	}
 
